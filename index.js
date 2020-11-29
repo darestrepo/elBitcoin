1acca5372872ffb45c045c13b1fc9874b3c1089a
Dario A Restrepo R <darioarestrepor@Darios-MacBook-Pro-6.local>
2020-11-29 08:14:33 -0500

    validando dialogflow

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 const express = require('express');
 const path = require('path');
 
+// Configurar db
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://alejoDB:dlkflsksdkjlkKaAS@cluster0.9ulgl.mongodb.net/bitcoindb?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 // Server
 var server = express();
-var port = process.env.PORT || 80; // <== this is must
+var port = process.env.PORT || 8082; // <== this is must
 
-server.get('/', (req, res) => {
+server.post('/', async (req, res) => {
 
-    res.send("Working")
-})
+    await client.connect();
+
+    const database = client.db('bitcoindb');
+    const collection = database.collection("personas");
+    const action = req.body.queryResult.action;
+    console.log(req.body.queryResult.action);
+    switch (action) {
+        case "default":
+            console.log(`Se recibió la acción ${action}`);
+            res.json({
+                fulfillmentText: `Se recibió la acción ${action}`
+            }); 
+            break;   
+        case "registro":
+            console.log("aqui código");
+            break;            
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* insertar info en db
+    const persona = {
+        nombre: "Alejandro",
+        ciudad: "Medellín",
+        pais: "Colombia",
+        id: 98660650,
+        email: "alejandro@xymilar.com"
+    };
+    const result = await collection.insertOne(persona);
+    console.log(persona);
+     
+    res.send(`Guardado con éxito ${result}`)
+    */
+
+
+});
+
+
+
 
 server.listen(port, () => {
     console.log("Listening on port: " + port)
